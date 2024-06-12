4c14db6267d520c14da56f71474df7807d10f898
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-06-12 23:59:49 +0700

    Верстка чата

--- app/src/main/java/com/example/messenger/PersonalChat.java
+++ app/src/main/java/com/example/messenger/PersonalChat.java
@@ -1,9 +1,6 @@
-
 package com.example.messenger;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Chats extends AppCompatActivity {
+public class PersonalChat extends AppCompatActivity {
 
-    private ImageButton btn_profile, btn_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_chats);
+        setContentView(R.layout.activity_personal_chat);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        btn_profile = findViewById(R.id.btn_profile);
-        btn_profile.setOnClickListener(v->{
-            Intent intent = new Intent(Chats.this, Profile.class);
-            startActivity(intent);
-            overridePendingTransition(0, 0); // Убрать анимацию перехода
-        });
-
-        btn_add = findViewById(R.id.btn_add);
-        btn_add.setOnClickListener(v->{
-
         });
     }
 }
